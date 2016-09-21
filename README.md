727dd645cdb093f6cb406c8dcd768300e0fa8942
Germán Carrillo <geotux_tuxman@linuxmail.org>
2016-09-21 18:15:35 -0500

    Uploading proper structure to work with the QGIS Resource Sharing plugin as well as the first Processing script: Export Composers of Multiple Projects.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # QGIS-Resources
-My collection of QGIS resources shared with the community
+My collection of QGIS resources shared with the community. Hope you find them useful!
+
+
+## Collections:
+
+### Processing Scripts (PyQGIS) by Germán Carrillo
+
+ - **Export_Composers_of_Multiple_Projects.py**
+   This script allows you to export the composers of all the QGIS projects that are inside a base folder.
+   It's based on the Maps Printer plugin by [Harrissou Sant-anna](https://github.com/DelazJ), which is a required to run the script.
+
+
---
+++ collections/processing_scripts/processing/Export_Composers_of_Multiple_Projects.py
@@ -0,0 +1,44 @@
+##Projects_folder=folder
+##Output_folder=folder
+##Extension=selection PDF format (*.pdf *.PDF);JPG format (*.jpg *.JPG);JPEG format (*.jpeg *.JPEG);TIF format (*.tif *.TIF);TIFF format (*.tiff *.TIFF);PNG format (*.png *.PNG);BMP format (*.bmp *.BMP);ICO format (*.ico *.ICO);PPM format (*.ppm *.PPM));XBM format (*.xbm *.XBM);XPM format (*.xpm *.XPM)
+##nomodeler
+
+import os.path
+import glob 
+import qgis
+from qgis.core import QgsProject
+from PyQt4.QtCore import QFileInfo
+from processing.core.GeoAlgorithmExecutionException import GeoAlgorithmExecutionException
+
+if not Projects_folder or not Output_folder:
+    raise GeoAlgorithmExecutionException("Please specify both, projects folder and output folder.")
+
+# Settings
+projectPaths = glob.glob( os.path.join( Projects_folder, '*.qgs' ) )
+formats=['.pdf','.jpg','.jpeg','.tif','.tiff','.png','.bmp','.ico','.ppm','.xbm','.xpm']
+count=0
+
+if not 'MapsPrinter' in qgis.utils.plugins:
+    raise GeoAlgorithmExecutionException("The 'Maps Printer' plugin  is required!")
+    
+mp = qgis.utils.plugins['MapsPrinter']
+project = QgsProject.instance()
+qgis.utils.iface.mapCanvas().setRenderFlag( False )
+extension = formats[Extension]
+
+# Do the work!
+for projectPath in projectPaths:
+    qgis.utils.iface.newProject() # Needed to reset composer manager
+    project.read( QFileInfo( projectPath ) )
+    progress.setInfo( projectPath + " project read!" )
+    progress.setPercentage( count * 100 / len( projectPaths ) ) 
+    count+=1
+    
+    for composer in qgis.utils.iface.activeComposers():
+        progress.setInfo( "    Composer found:  " + composer.composerWindow().windowTitle() )
+        title = composer.composerWindow().windowTitle()
+        title = project.fileInfo().baseName() + '_' + title
+        mp.exportCompo( composer, Output_folder, title, extension )
+        progress.setInfo( "        Composer exported!" )
+
+qgis.utils.iface.mapCanvas().setRenderFlag( True )
