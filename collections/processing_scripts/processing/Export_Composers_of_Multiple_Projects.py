07425ee930dddc93608b8df2c6ab91bfbcb0c6cd
gacarrillor <gcarrillo@linuxmail.org>
2019-08-07 19:47:38 -0500

    Migrate Export_Composers_of_Multiple_Projects to QGIS v3

--- collections/processing_scripts/processing/Export_Composers_of_Multiple_Projects.py
+++ collections/processing_scripts/processing/Export_Composers_of_Multiple_Projects.py
@@ -1,47 +1,121 @@
-##QGIS_Projects=group 
-##Batch_Export_Project_Composers=name
-##Projects_folder=folder
-##Output_folder=folder
-##Extension=selection PDF format (*.pdf *.PDF);JPG format (*.jpg *.JPG);JPEG format (*.jpeg *.JPEG);TIF format (*.tif *.TIF);TIFF format (*.tiff *.TIFF);PNG format (*.png *.PNG);BMP format (*.bmp *.BMP);ICO format (*.ico *.ICO);PPM format (*.ppm *.PPM));XBM format (*.xbm *.XBM);XPM format (*.xpm *.XPM)
-##nomodeler
-
-# Author: German Carrillo (GeoTux), 2016
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+                        Batch Export Project Composers
+                             --------------------
+        begin                : 2019-08-07
+        git sha              : :%H$
+        copyright            : (C) 2019 by Germán Carrillo (GeoTux)
+        email                : gcarrillo@linuxmail.org
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License v3.0 as          *
+ *   published by the Free Software Foundation.                            *
+ *                                                                         *
+ ***************************************************************************/
+"""
 import os.path
-import glob 
+import glob
 import qgis
-from qgis.core import QgsProject
-from PyQt4.QtCore import QFileInfo
-from processing.core.GeoAlgorithmExecutionException import GeoAlgorithmExecutionException
+from qgis.core import (QgsProject,
+                       QgsProcessing,
+                       QgsProcessingAlgorithm,
+                       QgsProcessingParameterFeatureSource,
+                       QgsProcessingParameterFolderDestination,
+                       QgsProcessingParameterVectorLayer,
+                       QgsProcessingParameterEnum,
+                       QgsProcessingParameterBoolean,
+                       QgsProcessingParameterString,
+                       QgsProcessingOutputVectorLayer)
+from qgis.PyQt.QtCore import QCoreApplication
 
-if not Projects_folder or not Output_folder:
-    raise GeoAlgorithmExecutionException("Please specify both, projects folder and output folder.")
 
-# Settings
-projectPaths = glob.glob( os.path.join( Projects_folder, '*.qgs' ) )
-formats=['.pdf','.jpg','.jpeg','.tif','.tiff','.png','.bmp','.ico','.ppm','.xbm','.xpm']
-count=0
+class BatchExportProjectComposers(QgsProcessingAlgorithm):
 
-if not 'MapsPrinter' in qgis.utils.plugins:
-    raise GeoAlgorithmExecutionException("The 'Maps Printer' plugin  is required!")
-    
-mp = qgis.utils.plugins['MapsPrinter']
-project = QgsProject.instance()
-qgis.utils.iface.mapCanvas().setRenderFlag( False )
-extension = formats[Extension]
+    PROJECTS_FOLDER = 'PROJECTS_FOLDER'
+    EXTENSION = 'EXTENSION'
+    OUTPUT_FOLDER = 'OUTPUT_FOLDER'
+    OUTPUT = 'OUTPUT'
 
-# Do the work!
-for projectPath in projectPaths:
-    qgis.utils.iface.newProject() # Needed to reset composer manager
-    project.read( QFileInfo( projectPath ) )
-    progress.setInfo( projectPath + " project read!" )
-    progress.setPercentage( count * 100 / len( projectPaths ) ) 
-    count+=1
-    
-    for composer in qgis.utils.iface.activeComposers():
-        progress.setInfo( "    Composer found:  " + composer.composerWindow().windowTitle() )
-        title = composer.composerWindow().windowTitle()
-        title = project.fileInfo().baseName() + '_' + title
-        mp.exportCompo( composer, Output_folder, title, extension )
-        progress.setInfo( "        Composer exported!" )
+    def createInstance(self):
+        return type(self)()
 
-qgis.utils.iface.mapCanvas().setRenderFlag( True )
+    def group(self):
+        return QCoreApplication.translate("BatchExportProjectComposers", "QGIS Projects")
+
+    def groupId(self):
+        return 'qgis_projects'
+
+    def tags(self):
+        return (QCoreApplication.translate("BatchExportProjectComposers", 'layout,composer,map,printer,batch')).split(',')
+
+    def __init__(self):
+        super().__init__()
+
+    def initAlgorithm(self, config=None):
+        self.addParameter(QgsProcessingParameterFolderDestination(self.PROJECTS_FOLDER,
+                                                              QCoreApplication.translate("BatchExportProjectComposers", "Projects folder")))
+        self.addParameter(QgsProcessingParameterEnum(self.EXTENSION,
+                                                              QCoreApplication.translate("BatchExportProjectComposers", "Extension of generated maps"),
+                                                              ['.pdf', '.jpg', '.jpeg', '.tif', '.tiff', '.png', '.bmp', '.ico', '.ppm', '.xbm', '.xpm']))
+        self.addParameter(QgsProcessingParameterFolderDestination(self.OUTPUT_FOLDER,
+                                                        QCoreApplication.translate("BatchExportProjectComposers", "Output folder where to save maps")))
+        self.addOutput(QgsProcessingOutputVectorLayer(self.OUTPUT,
+                                                        QCoreApplication.translate("BatchExportProjectComposers", 'Output')))
+
+    def name(self):
+        return 'BatchExportProjectComposers'
+
+    def displayName(self):
+        return QCoreApplication.translate("BatchExportProjectComposers", "Batch export project composers")
+
+    def flags(self):
+        return super().flags() | QgsProcessingAlgorithm.FlagNoThreading
+
+    def processAlgorithm(self, parameters, context, feedback):
+        Projects_folder = self.parameterAsString(parameters, self.PROJECTS_FOLDER, context)
+        Extension = self.parameterAsEnum(parameters, self.EXTENSION, context)
+        Output_folder = self.parameterAsString(parameters, self.OUTPUT_FOLDER, context)
+        Output = self.parameterAsVectorLayer(parameters, self.OUTPUT, context)
+
+        # Settings
+        projectPaths = glob.glob(os.path.join(Projects_folder, '*.qg[s|z]'))
+
+        if not len(projectPaths):
+            feedback.reportError("\nERROR: No QGIS project files (.qgs or .qgz) found in the specified folder. We cannot continue...\n")
+        elif not os.path.isdir(Output_folder):
+            feedback.reportError("\nERROR: No output folder given. We cannot continue...\n")
+        else:
+            formats = ['.pdf', '.jpg', '.jpeg', '.tif', '.tiff', '.png', '.bmp', '.ico', '.ppm', '.xbm', '.xpm']
+            count = 0
+            exported_count = 0
+
+            if not 'MapsPrinter' in qgis.utils.plugins:
+                feedback.reportError("\nERROR: The 'Maps Printer' plugin  is required!\n")
+            else:
+                mp = qgis.utils.plugins['MapsPrinter']
+                project = QgsProject.instance()
+                extension = formats[Extension]
+
+                # Do the work!
+                for projectPath in projectPaths:
+                    # qgis.utils.iface.newProject() # Needed to reset composer manager
+                    project.read(projectPath)
+                    feedback.pushInfo("\n'{}' project read!!".format(projectPath))
+                    feedback.setProgress(count * 100 / len(projectPaths))
+                    count += 1
+
+                    for composer in project.layoutManager().layouts():
+                        feedback.pushInfo("\n --> Composer found: '{}'!".format(composer.name()))
+                        title = composer.name()
+                        title = project.baseName() + '_' + title
+                        mp.exportCompo(composer, Output_folder, title, extension)
+                        feedback.pushInfo("      Composer exported!")
+                        exported_count += 1
+
+                if exported_count:
+                    feedback.pushInfo("\nINFO: Maps were saved in '{}'\n".format(Output_folder))
+
+        return {self.OUTPUT: Output}
