026ae3d3ca683091f9db0c2fe44693ca1c91a81d
Germán Carrillo <gcarrillo@linuxmail.org>
2022-02-23 20:40:24 -0500

    Optimize 'vector_overlaps_by_class.py' avoiding invalid geometries ('fix geometries' is costly)

--- collections/processing_scripts/processing/vector_overlaps_by_class.py
+++ collections/processing_scripts/processing/vector_overlaps_by_class.py
@@ -34,7 +34,8 @@ from qgis.core import (QgsProcessing,
                        QgsWkbTypes,
                        QgsDistanceArea,
                        QgsGeometry,
-                       QgsFeature)
+                       QgsFeature,
+                       QgsExpression)
 from qgis import processing
 
 
@@ -188,13 +189,6 @@ class CalculateVectorOverlapsByClass(QgsProcessingAlgorithm):
         if sink is None:
             raise QgsProcessingException(self.invalidSinkError(parameters, self.OUTPUT))
 
-        # build a spatial index for constraint layer for speed. We also store input 
-        # constraint geometries here, to avoid refetching and projecting them later
-        #feedback.pushInfo(self.tr("Preparing {}").format(overlay.name()))
-        #request = QgsFeatureRequest().setSubsetOfAttributes(list()).setDestinationCrs(source_crs, context.transformContext()).setInvalidGeometryCheck(context.invalidGeometryCheck())
-        #feature_it = overlay.getFeatures(request)
-        #spatial_index = QgsSpatialIndex(feature_it, feedback, QgsSpatialIndex.FlagStoreFeatureGeometries)
-
         da = QgsDistanceArea()
         da.setSourceCrs(source_crs, context.transformContext())
         da.setEllipsoid(context.ellipsoid())
@@ -202,90 +196,92 @@ class CalculateVectorOverlapsByClass(QgsProcessingAlgorithm):
         # loop through input
         step = 100.0 / source_count if source_count > 0 else 0
         i = 0
+        feedback.setProgress(1)
+
         for feature in source_features:
             if feedback.isCanceled():
                 break
 
             id_value = feature[id_field]
 
-            feedback.pushInfo('Preparing input feature with "{}" {}...'.format(id_field, id_value))
+            feedback.pushInfo('\n{}) Preparing input feature with "{}" {}...'.format(i+1, id_field, id_value))
 
             # 1) Get the current feature in its own layer
+            feedback.pushInfo('\nExtracting base polygon...')
             feature_layer = processing.run("native:extractbyattribute", {
                                            'INPUT':parameters[self.INPUT],
                                            'FIELD':id_field,
                                            'OPERATOR':0, # =
                                            'VALUE':id_value,
-                                           'OUTPUT':'TEMPORARY_OUTPUT'}, context=context, feedback=feedback)['OUTPUT']
+                                           'OUTPUT':'TEMPORARY_OUTPUT'}, context=context)['OUTPUT']
 
             if feedback.isCanceled():
                 break
 
             # 2) Extract all overlay polygons by location for the current feature
+            feedback.pushInfo('Getting class features by location...')
             overlay_subset = processing.run("native:extractbylocation", {
                                             'INPUT':overlay,
                                             'PREDICATE':[0],  # Intersects
                                             'INTERSECT':feature_layer,
-                                            'OUTPUT':'TEMPORARY_OUTPUT'}, context=context, feedback=feedback)['OUTPUT']
+                                            'OUTPUT':'TEMPORARY_OUTPUT'}, context=context)['OUTPUT']
 
             if feedback.isCanceled():
                 break
-
-            # 3) Collect all polygons of a class into a single polygon (fix geometries of the result)
-            class_layer = processing.run("native:collect", {
-                                         'INPUT': overlay_subset,
-                                         'FIELD':[class_field],
-                                         'OUTPUT':'TEMPORARY_OUTPUT'
-                                         }, context=context, feedback=feedback)['OUTPUT']
-
-            if feedback.isCanceled():
-                break
-
-            class_layer = processing.run("native:fixgeometries", {
-                                         'INPUT': class_layer,
-                                         'OUTPUT':'TEMPORARY_OUTPUT'
-                                         }, context=context, feedback=feedback)['OUTPUT']
-
-            if feedback.isCanceled():
-                break
-
-            class_layer_features = [feat for feat in class_layer.getFeatures()]
 
             out_features = list()
+            idx_class_field = overlay_subset.fields().indexOf(class_field)
             
-            feedback.pushInfo('Analysing overlaps in input feature with id {}...'.format(id_value))
+            feedback.pushInfo('\nAnalysing overlaps in input feature with id {}...'.format(id_value))
             
             if feature.hasGeometry() and not qgsDoubleNear(feature.geometry().area(), 0.0):
                 input_geom = feature.geometry()
                 input_area = da.measureArea(input_geom)
 
-                # prepare for lots of intersection tests (for speed)
+                # Prepare for lots of intersection tests (for speed)
                 geom_engine = QgsGeometry.createGeometryEngine(input_geom.constGet())
                 geom_engine.prepareGeometry()
 
-                # calculate overlap attributes
-                for class_feature in class_layer_features:
+                if feedback.isCanceled():
+                    break
+
+                # Iterate classes
+                unique_values = overlay_subset.uniqueValues(idx_class_field)
+                sub_step = step / len(unique_values) if unique_values else 0
+                j = 0
+
+                for class_name in unique_values:
+                    feedback.pushInfo("...Analysing class '{}'...".format(class_name))
+                    exp = QgsExpression("{} = '{}'".format(class_field, class_name))
+                    request = QgsFeatureRequest(exp).setSubsetOfAttributes(list()).setDestinationCrs(source_crs, context.transformContext()).setInvalidGeometryCheck(context.invalidGeometryCheck())
+
+                    class_overlay_area = 0
+                    
+                    # Iterate features by class
+                    for class_feature in overlay_subset.getFeatures(request):
+                        if feedback.isCanceled():
+                            break
+
+                        # Since we know in 2 we get all overlay features that intersect,
+                        # we skip the otherwise must-have intersects check and proceed
+                        # to get the intersection right away
+                        if class_feature.hasGeometry() and not qgsDoubleNear(class_feature.geometry().area(), 0.0):
+                            overlay_intersection = geom_engine.intersection(class_feature.geometry().constGet())
+                            class_overlay_area += da.measureArea(QgsGeometry(overlay_intersection))
+
                     if feedback.isCanceled():
                         break
-                    
-                    overlay_area = 0
-                    
-                    # Since we know in 2 we get all overlay features that intersect,
-                    # we skip the otherwise must-have intersects check and proceed
-                    # to get the intersection right away
-                    overlay_intersection = geom_engine.intersection(class_feature.geometry().constGet())
-                    overlay_area = da.measureArea(QgsGeometry(overlay_intersection))
-                        
-                    if feedback.isCanceled():
-                        break
-                    
+
                     out_feature = QgsFeature()
                     out_attrs = [id_value,
-                                 class_feature[class_field],
-                                 overlay_area,
-                                 100 * overlay_area / input_area]
+                                 class_name,
+                                 class_overlay_area,
+                                 100 * class_overlay_area / input_area]
                     out_feature.setAttributes(out_attrs)
                     out_features.append(out_feature)
+
+                    j += 1
+                    feedback.setProgress(i * step + j * sub_step)
             else:
                 # Input feature has no geometry
                 for class_feature in class_layer_features:
